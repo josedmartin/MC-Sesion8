d2476dc887be6d2f673f0ae435fbf10792fb9053
josedmartin <josedmartinr@gmail.com>
2022-12-15 19:54:20 +0100

    Cambios en resultadosv2

--- interfaz2.py
+++ interfaz2.py
@@ -67,7 +67,7 @@ class App(customtkinter.CTk):
         self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
         self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0, text="Función")
         self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1, text="Puntos Fijos")
+        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1, text="Resultados")
         self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
         self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=2, text="Órbita")
         self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
